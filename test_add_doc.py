55d3cf7dce02c2c1687e52a6b56a11c936a6b7f8
Stepan Krylov <stepan.krylov1991@yandex.ru>
2019-09-04 17:57:19 +0500

    rework

--- test_add_doc.py
+++ test_add_doc.py
@@ -4,14 +4,25 @@ import registrar
 
 
 class TestRegistrar(unittest.TestCase):
+    def setUp(self):
+        with open("fixture\documents.json", "r", encoding='utf-8') as file:
+            json_list = json.load(file)
+            list_test_2 = []
+            for item in json_list:
+                list_test_2.append(item)
+            self.y = list_test_2
 
-    def test_add_doc_1(self):
-        res = registrar.add_doc('0', '1', '2', '3')
-        self.assertEqual(len(res[0]), 3)
-    
-    def test_add_doc_2(self):
-        res = registrar.add_doc('0', '1', '2', '3')
-        self.assertEqual(len(res[1]), 5)
+        with open("fixture\directories.json", "r", encoding='utf-8') as file:
+            json_list = json.load(file)
+            list_test_1 = []
+            for item in json_list:
+                list_test_1.append(item)
+            self.z = list_test_1
 
+    def test_add_doc(self):
+        res = registrar.add_doc('0', '1', '2', '3')
+        self.assertEqual(len(res[0]), len(self.z))
+        self.assertEqual(len(res[1]), len(self.y)+1)
+        
 if __name__ == '__main__':
     unittest.main()
--- test_delete_doc.py
+++ test_delete_doc.py
@@ -4,14 +4,29 @@ import registrar
 
 
 class TestRegistrar(unittest.TestCase):
+    def setUp(self):
+        with open("fixture\documents.json", "r", encoding='utf-8') as file:
+            json_list = json.load(file)
+            list_test_2 = []
+            for item in json_list:
+                self.x = item["number"]
+                list_test_2.append(item)
+            self.y = list_test_2
+
+        with open("fixture\directories.json", "r", encoding='utf-8') as file:
+            json_list = json.load(file)
+            list_test_1 = []
+            for item in json_list:
+                list_test_1.append(item)
+            self.z = list_test_1
 
     def test_delete_doc_1(self):
-        res = registrar.delete_doc('10006')
-        self.assertEqual(len(res[0]), 3)
-    
+        res = registrar.delete_doc(self.x)
+        self.assertEqual(len(res[0]), len(self.z))
+
     def test_delete_doc_2(self):
-        res = registrar.delete_doc('10006')
-        self.assertEqual(len(res[1]), 2)
+        res = registrar.delete_doc(self.x)
+        self.assertEqual(len(res[1]), len(self.y)-1)
 
 if __name__ == '__main__':
     unittest.main()
